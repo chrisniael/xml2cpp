0e07903ea3c5a94f82191a27f370ff6908944d3d
shenyu <shenyu@shenyu.me>
2018-11-30 15:29:07 +0800

    Cmd arg parse complete.

--- src/main.cpp
+++ src/main.cpp
@@ -6,6 +6,7 @@
  * @date 2018-11-26
  */
 
+#include <getopt.h>
 #include <fstream>
 #include <iostream>
 #include <sstream>
@@ -168,16 +169,12 @@ std::string ParseChildNode(pugi::xml_node node, std::stringstream& ss) {
   std::string code_member_var_init;
   pugi::xml_node cur_child_node = node.first_child();
   while (cur_child_node && cur_child_node.type() == pugi::node_element) {
-    std::cout << "cur node=" << cur_child_node.name() << std::endl;
-
     // 检查重复的子节点
     bool has_same_child_node = false;
     pugi::xml_node next_same_sibling_node =
         cur_child_node.next_sibling(cur_child_node.name());
     while (next_same_sibling_node) {
       has_same_child_node = true;
-      std::cout << "next same node=" << next_same_sibling_node.name()
-                << std::endl;
       node.remove_child(next_same_sibling_node);
       next_same_sibling_node =
           cur_child_node.next_sibling(cur_child_node.name());
@@ -185,7 +182,6 @@ std::string ParseChildNode(pugi::xml_node node, std::stringstream& ss) {
 
     if (has_same_child_node) {
       // 处理 cur_child_node
-      ss << "// TODO:" << cur_child_node.name() << std::endl;
       code_member_var_init += ParseRepeatedNode(cur_child_node, ss);
 
       cur_child_node = cur_child_node.next_sibling();
@@ -205,8 +201,6 @@ std::string ParseNormalNode(pugi::xml_node node, std::stringstream& ss) {
   bool is_child_plain_data = false;
   for (const pugi::xml_node& child_node : node.children()) {
     if (child_node.type() == pugi::node_pcdata) {
-      ss << "// node text=" << node.text().as_string() << std::endl;
-      // ss << fmt::format("pugi::xml_text {}_;\n", node.name());
       is_child_plain_data = true;
       break;
     }
@@ -222,8 +216,6 @@ std::string ParseNormalNode(pugi::xml_node node, std::stringstream& ss) {
   std::string code_public_member_attr_define;
   std::string code_public_member_attr_init;
   for (const pugi::xml_attribute& node_attr : node.attributes()) {
-    std::cout << "// node_attr: " << node_attr.name() << "="
-              << node_attr.as_string() << std::endl;
     code_public_member_attr_define += AttrDefine(node_attr.name());
     code_public_member_attr_init += AttrInit(node_attr.name());
   }
@@ -268,8 +260,6 @@ std::string ParseRepeatedNode(pugi::xml_node node, std::stringstream& ss) {
   std::string code_public_member_attr_define;
   std::string code_public_member_attr_init;
   for (const pugi::xml_attribute& node_attr : node.attributes()) {
-    std::cout << "// node_attr: " << node_attr.name() << "="
-              << node_attr.as_string() << std::endl;
     code_public_member_attr_define += AttrDefine(node_attr.name());
     code_public_member_attr_init += AttrInit(node_attr.name());
   }
@@ -311,22 +301,80 @@ void ParseRootNode(const std::string& xml_example_name, pugi::xml_node node,
   ss << GetMemberVarFunc(xml_example_name);
 }
 
-int main() {
-  std::string xml_example_name = "gateserver";
+bool ParseXmlExampleFileName(const std::string& xml_example_file,
+                             std::string* xml_example_name) {
+  const auto file_suffix_pos = xml_example_file.rfind(".xml");
+  if (file_suffix_pos == std::string::npos || file_suffix_pos == 0) {
+    return false;
+  }
+
+  const auto last_slash_pos = xml_example_file.rfind("/");
+  if (last_slash_pos == std::string::npos) {
+    *xml_example_name = xml_example_file.substr(0, file_suffix_pos);
+  } else if (last_slash_pos + 1 >= file_suffix_pos) {
+    return false;
+  } else {
+    *xml_example_name = xml_example_file.substr(
+        last_slash_pos + 1, file_suffix_pos - last_slash_pos - 1);
+  }
+
+  return true;
+}
+
+enum Xml2CppRes {
+  Xml2CppResOk = 0,
+  Xml2CppResFileNameErr = 11,
+  Xml2CppResFileNotExist,
+  Xml2CppResOutDirInvlid,
+  Xml2CppResParseError,
+};
+
+std::string Xml2CppResDesc(const Xml2CppRes& xml2cpp_res) {
+  switch (xml2cpp_res) {
+    case (Xml2CppResOk):
+      return "Ok";
+      break;
+    case (Xml2CppResFileNameErr):
+      return "Filename invalid.";
+      break;
+    case (Xml2CppResFileNotExist):
+      return "File nost exist.";
+      break;
+    case (Xml2CppResOutDirInvlid):
+      return "Out directory invalid.";
+      break;
+    case (Xml2CppResParseError):
+      return "Parse xml error.";
+      break;
+    default:
+      return "";
+      break;
+  }
+}
+
+Xml2CppRes Xml2Cpp(const std::string& xml_example_file,
+                   const std::string& out_dir) {
+  // TODO: 判断目录是否有效
+
+  std::string xml_example_name;
+  if (!ParseXmlExampleFileName(xml_example_file, &xml_example_name)) {
+    return Xml2CppResFileNameErr;
+  }
 
   pugi::xml_document xml_doc;
-  pugi::xml_parse_result xml_load_result = xml_doc.load_file(
-      fmt::format("{}.xml.example", xml_example_name).c_str());
-  if (!xml_load_result) {
-    std::cerr << "Err: " << xml_load_result.description() << std::endl;
-    return 1;
+  pugi::xml_parse_result xml_load_result =
+      xml_doc.load_file(xml_example_file.c_str());
+  if (xml_load_result.status == pugi::status_file_not_found) {
+    return Xml2CppResFileNotExist;
+  } else if (!xml_load_result) {
+    return Xml2CppResParseError;
   }
 
   std::stringstream ss;
   ParseRootNode(xml_example_name, xml_doc, ss);
 
   std::ofstream fout;
-  fout.open(fmt::format("src/{}.xml.h", xml_example_name),
+  fout.open(fmt::format("{}/{}.xml.h", out_dir, xml_example_name),
             std::ios_base::out | std::ios_base::trunc);
 
   fout << FileMacroDefineBeg(xml_example_name);
@@ -344,4 +392,78 @@ int main() {
   fout << NamespaceDefineEnd("config");
   fout << std::endl;
   fout << FileMacroDefineEnd(xml_example_name);
+
+  return Xml2CppResOk;
+}
+
+std::string HelpInfo() { return "Usage: xml2cpp [--out DIR] --file FILE"; }
+
+enum ParseCmdLineRes {
+  ParseCmdLineResOK = 0,
+  ParseCmdLineResErr = 1,
+  ParseCmdLineResEnd = 2
+};
+
+ParseCmdLineRes ParseCmdLine(int argc, char* argv[],
+                             std::string* xml_example_file,
+                             std::string* out_dir) {
+  const char* optstring = "";
+  int c;
+  struct option opts[] = {{"out", optional_argument, nullptr, 'o'},
+                          {"file", required_argument, nullptr, 'f'},
+                          {"help", no_argument, nullptr, 'h'},
+                          {nullptr, 0, nullptr, '?'}};
+  while ((c = getopt_long(argc, argv, optstring, opts, nullptr)) != -1) {
+    switch (c) {
+      case 'o':
+        if (!optarg) {
+          return ParseCmdLineResErr;
+        }
+        *out_dir = optarg;
+        break;
+      case 'f':
+        if (!optarg) {
+          return ParseCmdLineResErr;
+        }
+        *xml_example_file = optarg;
+        break;
+      case 'h':
+        return ParseCmdLineResEnd;
+        break;
+      case '?':
+        return ParseCmdLineResErr;
+        break;
+      default:
+        return ParseCmdLineResErr;
+        break;
+    }
+  }
+
+  if (xml_example_file->size() > 0 && out_dir->size() > 0) {
+    return ParseCmdLineResOK;
+  } else {
+    return ParseCmdLineResErr;
+  }
+}
+
+int main(int argc, char* argv[]) {
+  std::string xml_example_file;
+  std::string out_dir;
+  int parse_cmd_line_res =
+      ParseCmdLine(argc, argv, &xml_example_file, &out_dir);
+  if (parse_cmd_line_res == ParseCmdLineResOK) {
+    Xml2CppRes xml2cpp_res = Xml2Cpp(xml_example_file, out_dir);
+    if (xml2cpp_res != Xml2CppResOk) {
+      std::cerr << fmt::format("Err: {}", Xml2CppResDesc(xml2cpp_res))
+                << std::endl;
+      return xml2cpp_res;
+    }
+  } else if (parse_cmd_line_res == ParseCmdLineResEnd) {
+    std::cout << HelpInfo() << std::endl;
+  } else {
+    std::cout << HelpInfo() << std::endl;
+    return parse_cmd_line_res;
+  }
+
+  return 0;
 }
